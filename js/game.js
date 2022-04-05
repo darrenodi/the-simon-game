1ef62627531cc4321062e213b84f4d10682a69df
darrenodi <tantheta85@gmail.com>
2022-04-05 21:36:03 +0100

    animate press

--- js/game.js
+++ js/game.js
@@ -9,21 +9,37 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 // next sequence function
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4) + 0;
-  return randomNumber;
+  var randomChosenColour = buttonColors[randomNumber];
+  //add color to pattern
+  gamePattern.push(randomChosenColour);
+  var nextAudio = new Audio('sounds/'+randomChosenColour+".mp3");
+  nextAudio.play();
+
+  //Jquery event listener and flash
+
+  var s = $("div."+randomChosenColour);
+
+  s.stop().fadeOut(200).fadeIn(200);
+
+  $("div.btn").on( "click", this.id , function() {
+    var userChosenColour  = this.id;
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    console.log()
+    userClickedPattern.push(userChosenColour);
+  });
+}
+nextSequence();
+
+//play pressed sound function
+function playSound(name){
+  var playAudio = new Audio('sounds/'+name+".mp3");
+  playAudio.play();
 }
 
-//chosen color
-var randomChosenColour = buttonColors[nextSequence()];
-
-//add color to pattern
-gamePattern.push(randomChosenColour);
-
-//Jquery event listener and flash
-var s = $("div."+randomChosenColour);
-
-s.stop().fadeOut(200).fadeIn(200);
-
-$("div.btn").on( "click", this.id , function() {
-  var userChosenColour  = this.id;
-  userClickedPattern.push(userChosenColour);
+function animatePress(currentColour){
+  this.css({'box-shadow': '10px 10px 5px #888'
+,
+  'background-color' : 'grey',
 });
+}
